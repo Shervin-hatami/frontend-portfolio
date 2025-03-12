268302bd5f10cdaaa5cfe739255346bbabaef57e
shervin hatami <hatamishervin13@gmail.com>
2025-03-12 18:56:46 +0100

    fix branch

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import PageTitle from '../components/PageTitle'
 export default async function Home() {
   return (
     <main className="min-h-screen p-8">
-      <div className="flex justify-center items-center p-10">
+      <div className="flex justify-center items-center p-8">
       <PageTitle />
       </div>
       <JobList />
